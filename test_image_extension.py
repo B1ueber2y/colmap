a2eac484a5ac20a1a0ce6580dc01881208086c69
B1ueber2y <b1ueber2y@gmail.com>
2024-12-16 00:18:47 +0100

    update test script

--- test_image_extension.py
+++ test_image_extension.py
@@ -1,7 +1,6 @@
 import pycolmap
 
 image = pycolmap.create_test_image()
-extension = image.get_extension()
-print(type(extension))
-print(extension.test_field)
+print(type(image.extension))
+print(image.extension.test_field)
 
